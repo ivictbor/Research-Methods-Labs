a48b221c3c9136ca5e95f743e8aa563452fe9118
Vanya <ivictbor@gmail.com>
2014-03-23 17:19:21 +0400

    graph + data files

--- single2d_regr.py
+++ single2d_regr.py
@@ -19,8 +19,9 @@ try:
         match = re.match('^(\S+)\s+(\S+)\s+(\S+)$', line)
         if match and re.match('\d+', match.group(1)):  #second cond: if first column is number, not 'sum' or smth else
             data.append( {'N':match.group(1),
-                          'Y':float(match.group(2)),
-                          'X':float(match.group(3))} )
+                          'Y':float(match.group(2).replace(',','.')), 
+                          'X':float(match.group(3).replace(',','.'))
+                         } )
 finally:
     dataFile.close()
    
@@ -56,7 +57,10 @@ print ('Variance of error(6_E^2): {}'.format(round(sqVarE,prec)))
 sqR = (sqVar - sqVarE)/sqVar;
 
 # coefficient of correlation 
-rXY = math.sqrt(sqR);
+if sqR<0:
+    rXY = 0;
+else:   
+    rXY = math.sqrt(sqR);
 
 print ('Coefficient of determination(R^2): {}'.format(round(sqR,prec)))
 print ('Coefficient of correlation(r_XY): {}'.format(round(rXY,prec)))
@@ -92,17 +96,17 @@ print ('Elasticity of Y depend on X: {}'.format(round(Elxy,prec)))
 import matplotlib.pyplot as plt
 
 tex = '$Регрисионная\ модель:$\n\
-$Y_{ti} = '+str(round(b0,prec))+' + '+str(round(b1,prec))+' X_i$\n\n\
+$Y_{ti} = '+str(round(b0,prec))+(' + ' if b1 > 0 else ' - ')+str(round(abs(b1),prec))+' X_i$\n\n\
 $Дисперсия\ зависимой\ переменной\ Y:$\n\
 $\\sigma_Y^2='+str(round(sqVar,prec))+'$\n\n\
 $Дисперсия\ ошибок\ e:$\n\
-$\\sigma_e^2='+str(round(sqVarE,prec))+'$\n\n\
-$Коэфициент\ детерминации:$\n\
+$\\sigma_e^2='+str(round(sqVarE,prec))+'$\n\n'
+
+texr = '$Коэфициент\ детерминации:$\n\
 $R^2='+str(round(sqR,prec))+'$\n\n\
 $Коэфициент\ корелляции:$\n\
-$r_{XY}='+str(round(rXY,prec))+'$\n\n'
-
-texr = '$Стандартные\ погрешности\ оценок$\n\
+$r_{XY}='+str(round(rXY,prec))+'$\n\n\
+$Стандартные\ погрешности\ оценок$\n\
 $(с\ учетом\ дисперсии\ остатков)$\n\
 $S_{b_0}='+str(round(Sb0,prec))+'$\n\
 $S_{b_1}='+str(round(Sb1,prec))+'$\n\n\
@@ -115,15 +119,15 @@ $Эластичность\ Y\ в\ зависимости\ от\ X$\n\
 $Є_{Y/X}='+str(round(Elxy,prec))+'$\n\n'
 
 # draw area
-fig = plt.figure()
+fig = plt.figure(1)
 ax = fig.add_axes([0,0,1,1])
-ax.set_axis_off()
+ax.set_axis_on()
 
 # Set a possibility to draw cyrrilic fonts
 plt.rcParams["text.usetex"] = False
 
 # draw left side
-t = ax.text(0.25, 0.5, tex,
+t = ax.text(0.25, 0.75, tex,
         horizontalalignment='center',
         verticalalignment='center',
         fontsize=16, 
@@ -135,6 +139,28 @@ t = ax.text(0.75, 0.5, texr,
         verticalalignment='center',
         fontsize=16, 
         color='black')    
+
+import numpy as np       
+
+# plot graphic
+miny = min(d['Y'] for d in data)*0.9
+maxy = max(d['Y'] for d in data)*1.1    
+minx = min(d['X'] for d in data)*0.9
+maxx = max(d['X'] for d in data)*1.1  
+    
+t1 = np.arange(miny,maxy, (maxy - miny)/100)
+t2 = np.arange(minx,maxx, (maxx - minx)/100) 
+ax2 = plt.axes([0.12, 0.12, 0.28, 0.45])
+ax2.set_ylim(miny,maxy)
+ax2.set_xlim(minx,maxx)
+ax2.grid(True)
+ax2.plot(t2, b0 + b1*t2, 'r')     
+
+# arrays with points
+xarr = [d['X'] for d in data]  
+yarr = [d['Y'] for d in data]  
+
+plt.plot(xarr, yarr, 'bo')   # plot points from table
         
 # det size
 ax.figure.canvas.draw()
@@ -144,5 +170,6 @@ bbox = t.get_window_extent()
 fig.set_size_inches(bbox.width*2/80,bbox.height/80) # dpi=80
 
 plt.show()
+
 #plt.savefig('test.svg')
 #plt.savefig('test.png', dpi=300)
